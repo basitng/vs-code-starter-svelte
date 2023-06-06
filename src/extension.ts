29e270e4bc673dc3e2d4e6030663978d7f999cfa
basitng <basitng2004@gmail.com>
2023-06-06 09:35:11 +0100

    added sidebar, authenticate and gloal state

--- src/extension.ts
+++ src/extension.ts
@@ -2,8 +2,37 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 import { HelloWorldPanel } from "./HelloWorldPanel";
+import { SidebarProvider } from "./SidebarPanel";
+import { TokenManager } from "./TokenManager";
 
 export function activate(context: vscode.ExtensionContext) {
+  TokenManager.globalState = context.globalState;
+
+  const sidebarProvider = new SidebarProvider(context.extensionUri);
+
+  const item = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Left
+  );
+  item.text = "$(beaker) Add Todo";
+  item.command = "vstodo.addTodo";
+  item.show();
+
+  context.subscriptions.push(
+    vscode.window.registerWebviewViewProvider("vstodo-sidebar", sidebarProvider)
+  );
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand("vstodo.addTodo", () => {
+      vscode.commands.executeCommand("vstodo.addTodo");
+      const { activeTextEditor } = vscode.window;
+      if (!activeTextEditor) {
+        vscode.window.showInformationMessage("No text selected");
+        return;
+      }
+      const value = activeTextEditor.document.getText();
+      vscode.window.showInformationMessage(value);
+    })
+  );
   context.subscriptions.push(
     vscode.commands.registerCommand("vstodo.helloWorld", () => {
       HelloWorldPanel.createOrShow(context.extensionUri);
@@ -11,18 +40,14 @@ export function activate(context: vscode.ExtensionContext) {
   );
 
   context.subscriptions.push(
-    vscode.commands.registerCommand("vstodo.askQuestion", async () => {
-      const answer = await vscode.window.showInformationMessage(
-        "How was your day?",
-        "good",
-        "bad"
-      );
-
-      if (answer === "bad") {
-        vscode.window.showInformationMessage("Sorry to hear that");
-      } else {
-        console.log({ answer });
-      }
+    vscode.commands.registerCommand("vstodo.refresh", () => {
+      HelloWorldPanel.kill();
+      HelloWorldPanel.createOrShow(context.extensionUri);
+      setTimeout(() => {
+        vscode.commands.executeCommand(
+          "workbench.action.webview.openDeveloperTools"
+        );
+      }, 500);
     })
   );
 }
---
+++ webviews/components/Sidebar.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import type { User } from "../types";
+   
+</script>
+
+<h1>Hello world</h1>
